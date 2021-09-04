7ab5b6c288fb6f255f88aecf00ff62f5b08dce23
gutaors <gutaors@gmail.com>
2021-09-04 11:20:47 -0300

    [python 3] adicionei uma porrada de pastas design patterns etc

--- PYTHON_3_BASICO_AO_AVANCADO/decorators/decorador.py
+++ PYTHON_3_BASICO_AO_AVANCADO/decorators/decorador.py
@@ -1,14 +1,19 @@
 # decorador serve para adicionar alguma funcionalidade na função
-#por exemplo, calcular quanto tempo demorou para rodar
+# por exemplo, calcular quanto tempo demorou para rodar
 
 def velocidade(funcao):
     def escrava(*args, **kwargs):
         return funcao(*args, **kwargs)
-return escrava
+    return escrava
+
 
 
 @velocidade
-def demora():
-    for i in range(5):
-        sleep(1)
+def demora(qtd):
+    """ Função decorada """
+    for i in range(qtd):
+        print(i, end='')
 
+
+# Executa a função decorada
+demora(10000)
